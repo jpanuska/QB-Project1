226a994657bab75dff9aa580c199b7a65ad51955
sergeyradkov <radkovsn@gmail.com>
2016-06-22 15:34:32 -0600

    12

---
+++ public/component/user.js
@@ -0,0 +1,8 @@
+angular.module('qbhelper').component('customerComponent', {
+    templateUrl: '/component/customer.html',
+    controllerAs: 'vm',
+    controller: function ($state) {
+
+        var vm = this;
+    }
+});
