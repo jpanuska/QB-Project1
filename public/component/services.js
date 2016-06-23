c1b797d02635ba13d4283e9d69d38c67ed1de8b7
sergeyradkov <radkovsn@gmail.com>
2016-06-23 16:59:08 -0600

    123

--- public/component/services.js
+++ public/component/services.js
@@ -14,6 +14,13 @@ angular.module('qbhelper').
         }
     })
 
+    service('UserService', function($http, $state){
+
+        this.createLink = function(user){
+            console.log(user);
+        }
+    })
+
     .service('PhoneService', function () {
 
         this.formatPhoneNumber = function (tel) {
--- public/component/user.js
+++ public/component/user.js
@@ -1,8 +1,29 @@
 angular.module('qbhelper').component('userComponent', {
     templateUrl: '/component/user.html',
     controllerAs: 'vm',
-    controller: function ($state) {
+    controller: function ($state, UserService) {
 
         var vm = this;
+        vm.checked = false;
+
+        vm.find = function(user) {
+        // CHECK THE USER
+            vm.checked = true;
+        }
+        vm.createUser = function(newUser) {
+            UserService.createLink(newUser).then(handleSuccess, handleError);
+        }
+
+        function handleSuccess(res) {
+            if (res.data) {
+                ch.link = res.data;
+            } else {
+                ch.link = "Sorry, but there is some error.";
+            }
+        }
+        function handleError(err) {
+            console.log("SERVER ERROR ");
+            ch.link = "Sorry, but there is some error on Quickbooks server.";
+        }
     }
 });
